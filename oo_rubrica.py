0acd32432155e5833d0acddc9c6964a861f4bc1e
paolo <paolo@foo.com>
2024-01-21 23:43:12 +0100

    aggiunto richiesta conferma rimozione puntuale

--- oo_rubrica.py
+++ oo_rubrica.py
@@ -35,6 +35,7 @@ class Rubrica:
         self.lista = [x for x in self.lista if x not in contatti_da_rimuovere]
         self.modificata = True
 
+
 class BrowserRubrica:
     def __init__(self, rubrica):
         # TODO: inserire controlli per validazione del file
@@ -45,7 +46,7 @@ class BrowserRubrica:
     def getComando(self):
         print(
             f"\nrubrica: { self.rubrica.nomeFile} \n"
-            + "[ L/l lista contatti    ] [ LS/ls lista per gruppi ] [ C/c cerca contatto/i ]\n"
+            + "[ L/l lista contatti    ] [ LG/lg lista per gruppi ] [ C/c cerca contatto/i ]\n"
             + "[ A/a aggiungi contatto ] [ R/r Rimuovi Contatto   ] [ S/s Salva            ] [ E/Q/e/q Exit ]"
         )
 
@@ -53,7 +54,7 @@ class BrowserRubrica:
 
         if scelta in "Ll":
             return Comando_stampa_completa(self.rubrica)
-        elif scelta in ["LS", "ls", "Ls", "lS"]:
+        elif scelta in ("LG", "lg", "Lg", "lG"):
             return Comando_stampa_a_gruppi(self.rubrica, 5)
         elif scelta in "Aa":
             return Comando_aggiungi(self.rubrica)
@@ -67,6 +68,7 @@ class BrowserRubrica:
             return Comando_salva(self.rubrica)
         else:
             return Comando_non_riconosciuto(self.rubrica)
+
 
 class Comando:
     lastRicerca = ""
@@ -82,18 +84,29 @@ class Comando:
         print("\n")
         for riga in contatti:
             if riga is not None:
-            # print ( ' ,'.join( riga))
-                print( *riga, sep=", ")
+                # print ( ' ,'.join( riga))
+                print(*riga, sep=", ")
 
     def seleziona_contatti(self, messaggio, stringaDaRicercare):
         pass
 
-    def input_con_msg_e_default(self, messaggio, default):
-        risposta = input(messaggio + " [" + f"{default}" + "] ")
-        return default if risposta == "" else risposta
+    def input_con_msg_default_valori_ammessi(
+        self, messaggio, default, valoriAmmessi=""
+    ):
+        risposta = input(f"{messaggio} {valoriAmmessi} [{default}] =")
+        risposta = default if risposta == "" else risposta
+        if valoriAmmessi == "":
+            return risposta
+        while risposta not in valoriAmmessi:
+            print("risposta non ammessa")
+            risposta = input(f"{messaggio} {valoriAmmessi} [{default}] =")
+            risposta = default if risposta == "" else risposta
+
+        return risposta
 
     def exec(self):
         pass
+
 
 class Comando_stampa_completa(Comando):
     def __init__(self, rubrica) -> None:
@@ -101,6 +114,7 @@ class Comando_stampa_completa(Comando):
 
     def exec(self):
         self.stampa_contatti(self.rubrica.lista)
+
 
 class Comando_stampa_a_gruppi(Comando):
     def __init__(self, rubrica, dimensioneGruppo=5) -> None:
@@ -112,16 +126,18 @@ class Comando_stampa_a_gruppi(Comando):
         args = [iter(self.rubrica.lista)] * self.dimensioneGruppo
         gruppi = zip_longest(*args, fillvalue=None)
 
-        for gruppo in gruppi: 
+        for gruppo in gruppi:
             print("\n---------------------------------------------- ")
             self.stampa_contatti(gruppo)
 
-            if (
-                self.input_con_msg_e_default("\nancora ? ( s/S/n/N )", "s")
-                not in "sS"
-            ):
+            risposta = self.input_con_msg_default_valori_ammessi(
+                "\nancora ?", "s", ("s", "S", "n", "N")
+            )
+            if risposta in ("n", "N"):
                 break
+
         print("\n stampa per gruppi completata")
+
 
 class Comando_aggiungi(Comando):
     def __init__(self, rubrica) -> None:
@@ -146,12 +162,13 @@ class Comando_aggiungi(Comando):
 
         self.rubrica.aggiunge_contatto(contatto)
 
+
 class Comando_cerca_contatti(Comando):
     def __init__(self, rubrica) -> None:
         super().__init__(rubrica)
 
     def exec(self):
-        stringaDaRicercare = self.input_con_msg_e_default(
+        stringaDaRicercare = self.input_con_msg_default_valori_ammessi(
             "Inserisci stringa di ricerca per i contatti da visualizzare:",
             self.lastRicerca,
         )
@@ -164,41 +181,61 @@ class Comando_cerca_contatti(Comando):
         else:
             print("\n nulla da visualizzare \n")
 
+
 class Comando_rimuovi(Comando):
     def __init__(self, rubrica) -> None:
         super().__init__(rubrica)
 
     def exec(self):
-        stringaDaRicercare = self.input_con_msg_e_default(
+        stringaDaRicercare = self.input_con_msg_default_valori_ammessi(
             "Inserisci stringa di ricerca per i contatti da rimuovere:",
             self.lastRicerca,
         )
         self.aggiorna_last_ricerca(stringaDaRicercare)
 
         contattiDaRimuovere = self.rubrica.ricerca_contatti(stringaDaRicercare)
-        if contattiDaRimuovere:
+        if not contattiDaRimuovere:
+            print("nulla da rimuovere")
+        else:
             print("ATTENZIONE I SEGUENTI CONTATTI SOTTOELENCATI VERRANNO CANCELLATI")
             self.stampa_contatti(contattiDaRimuovere)
-            print(
-                "ATTENZIONE I SEGUENTI CONTATTI SOPRAELENCATI CONTATTI VERRANNO CANCELLATI"
+            print("ATTENZIONE I SOPRAELENCATI CONTATTI CONTATTI VERRANNO CANCELLATI")
+
+            risposta = self.input_con_msg_default_valori_ammessi(
+                "Sei sicuro di voler procedere? ", "N", ("s", "S", "n", "N", "tutti")
             )
 
-            risposta = self.input_con_msg_e_default("Sei sicuro di voler procedere (sS/nN) ?","N")
-            if risposta in ("S", "s"):
+            if risposta == "tutti":
                 self.rubrica.rimuove_contatti(contattiDaRimuovere)
                 print("contatti rimossi")
-        else:
-            print("nulla da rimuovere")
+            elif risposta in ("s", "S"):
+                for contatto in contattiDaRimuovere:
+                    risposta = self.input_con_msg_default_valori_ammessi(
+                        f"rimuovo il contatto { contatto } \n confermi?",
+                        "n",
+                        ("s", "S", "n", "N", "esci"),
+                    )
+                    if risposta == "esci":
+                        return
+                    if risposta in ("n", "N"):
+                        continue
+                    # risposta in ("s","S")
+                    self.rubrica.rimuove_contatti(
+                        [contatto]
+                    )  # TODO vedi se è il caso di migliorare
+                    print("contatto rimosso ")
+
 
 class Comando_salva(Comando):
     def __init__(self, rubrica) -> None:
         super().__init__(rubrica)
 
     def exec(self):
-        nomeFile = self.input_con_msg_e_default(
-            "\n nome con cui salvare ", self.rubrica.nomeFile
+        nomeFile = self.input_con_msg_default_valori_ammessi(
+            "\n nome con cui salvare ", self.rubrica.nomeFile, ""
         )
         self.rubrica.salva(nomeFile)
+
 
 class Comando_non_riconosciuto(Comando):
     def __init__(self, rubrica) -> None:
@@ -206,6 +243,7 @@ class Comando_non_riconosciuto(Comando):
 
     def exec(self):
         print("comando non riconosciuto")
+
 
 class Comando_exit(Comando):
     def __init__(self, rubrica) -> None:
@@ -216,11 +254,13 @@ class Comando_exit(Comando):
         if not self.rubrica.modificata:
             return
 
-        risposta = input(" La rubrica è stata modificata, salvo su disco ? (s/n) ")
-        while risposta not in "SsNn":
-            risposta = input(" La rubrica è stata modificata, salvo su disco ? (s/n) ")
+        risposta = self.input_con_msg_default_valori_ammessi(
+            "La rubrica è stata modificata, salvo su disco?", "s", ("s", "S", "n", "N")
+        )
 
-        self.rubrica.salva(self.rubrica.nomeFile)
+        if risposta in ("s", "S"):
+            self.rubrica.salva(self.rubrica.nomeFile)
+
 
 class Comando_foo(Comando):
     def __init__(self, rubrica) -> None:
