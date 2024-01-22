d1600bff6a8a18c8dcc2debc1740fd1e9d1421d4
paolo <paolo@foo.com>
2024-01-22 14:01:18 +0100

    aggiunto comando fake cercaXcolonne

--- oo_rubrica.py
+++ oo_rubrica.py
@@ -47,6 +47,7 @@ class BrowserRubrica:
         print(
             f"\nrubrica: { self.rubrica.nomeFile} \n"
             + "[ L/l lista contatti    ] [ LG/lg lista per gruppi ] [ C/c cerca contatto/i ]\n"
+            + "[ CC/cc cerca contatto x colonne] \n"            
             + "[ A/a aggiungi contatto ] [ R/r Rimuovi Contatto   ] [ S/s Salva            ] [ E/Q/e/q Exit ]"
         )
 
@@ -58,8 +59,11 @@ class BrowserRubrica:
             return Comando_stampa_a_gruppi(self.rubrica)
         elif scelta in "Aa":
             return Comando_aggiungi(self.rubrica)
-        elif scelta in "Cc":
+        elif scelta in ("C","c"):
             return Comando_cerca_contatti(self.rubrica)
+        elif scelta in ("CC","cc"):
+            return Comando_cerca_contatti_per_colonne(self.rubrica)
+        
         elif scelta in "Rr":
             return Comando_rimuovi(self.rubrica)
         elif scelta in "EeQq":
@@ -73,6 +77,8 @@ class BrowserRubrica:
 class Comando:
     lastRicerca = ""
     lastDimensioneGruppo = ""
+
+    lastRicercaPerColonne = ("","","","","")
 
     def aggiorna_last_ricerca(classe, stringa):
         Comando.lastRicerca = stringa
@@ -192,6 +198,13 @@ class Comando_cerca_contatti(Comando):
         else:
             print("\n nulla da visualizzare \n")
 
+class Comando_cerca_contatti_per_colonne(Comando):
+    def __init__(self, rubrica) -> None:
+        super().__init__(rubrica)
+
+    def exec(self):
+
+        return
 
 class Comando_rimuovi(Comando):
     def __init__(self, rubrica) -> None:
